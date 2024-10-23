8fcb0c1de59e63a068dfc68f3b1ed874c570bb07
rizwanda <rizwanda140102@gmail.com>
2024-10-23 20:36:05 +0700

    dashboard_admin.css

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -49,6 +49,8 @@ $result = mysqli_query($conn, $query);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="dashboard_admin.css">
+
 </head>
 <body>
     <div class="container">
